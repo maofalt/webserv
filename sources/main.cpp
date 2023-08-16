4dd8dba2633f01f7b3377f729a0e1cf844843b52
Zachary NOGUEIRA <znogueir@paul-f3Ar6s7.clusters.42paris.fr>
2023-08-16 23:16:35 +0200

    trying to close stuff, implemented sigint to quit but leaks

--- sources/main.cpp
+++ sources/main.cpp
@@ -17,6 +17,7 @@
 #include <cstring>
 #include <netdb.h>
 #include <errno.h>
+#include <signal.h>
 #include <unistd.h>
 #include <iostream>
 #include <stdlib.h>
@@ -28,6 +29,20 @@
 #define PORT "8694"
 #define BACKLOG 5
 #define BUFFER_SIZE 2048
+
+// volatile sig_atomic_t stop_flag = 0;
+
+// void signal_handler(int signum) {
+//     stop_flag = 1;
+// }
+
+volatile sig_atomic_t	run = 1;
+
+void	signal_handler(int sig)
+{
+	if (sig == SIGINT)
+	run = 0;
+}
 
 
 /*
@@ -44,6 +59,7 @@ QUESTION : Method binds to all addresses returned by getaddrinfo. And not jsut
 		each time it binds to a new address.
 		Shouldn't it just bind to the first address returned by getaddrinfo?
 */
+
 int	set_and_bind_sock_listen(int *sock_listen)
 {
 	struct addrinfo	hints;
@@ -101,55 +117,56 @@ QUESTION :
 */
 
 int setUpSocket(int* sock_listen) {
-    return set_and_bind_sock_listen(sock_listen);
+	return set_and_bind_sock_listen(sock_listen);
 }
 
 const int MAX_EVENTS = 10;  // Number of maximum events to be returned by epoll_wait to transfom in a define
 
 int setUpEpoll(int sock_listen) {
-    int epoll_fd = epoll_create1(0);
-    if (epoll_fd == -1) {
-        perror("epoll_create1");
-        exit(EXIT_FAILURE);//this exit is not safe!!!
-    }
+	int epoll_fd = epoll_create1(0);
+	if (epoll_fd == -1) {
+		// perror("epoll_create1");
+		// exit(EXIT_FAILURE); //this exit is not safe!!!
+		return perror("epoll_create1"), -1;
+	}
 
-    struct epoll_event ev;
-    ev.events = EPOLLIN;
-    ev.data.fd = sock_listen;
+	struct epoll_event ev;
+	ev.events = EPOLLIN;
+	ev.data.fd = sock_listen;
 
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock_listen, &ev) == -1) {
-        perror(strerror(errno));
-        exit(EXIT_FAILURE);//this exit is not safe!!!
-    }
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock_listen, &ev) == -1) {
+		// perror(strerror(errno));
+		// exit(EXIT_FAILURE); //this exit is not safe!!!
+		return perror(strerror(errno)), -1;
+	}
 
-    return epoll_fd;
+	return epoll_fd;
 }
 
 int accept_new_client(int epoll_fd, int sock_listen) {
-    
+	
 	struct sockaddr_storage	client_addr;
 	struct epoll_event 		event;
-    socklen_t 				client_addr_size;
+	socklen_t 				client_addr_size;
 	int 					sock_server;
 	
 	client_addr_size = sizeof(client_addr);
-    sock_server = accept(sock_listen, (struct sockaddr *)&client_addr, &client_addr_size);
-    if (sock_server == -1) {
-        perror("accept");
-        return -1;
-    }
+	sock_server = accept(sock_listen, (struct sockaddr *)&client_addr, &client_addr_size);
+	if (sock_server == -1) {
+		perror("accept");
+		return -1;
+	}
 
-    std::cout << "New client connected on descriptor " << sock_server << "!" << std::endl;
-    
-    event.events = EPOLLIN;
-    event.data.fd = sock_server;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock_server, &event) == -1) {
-        perror("epoll_ctl: sock_server");
-        close(sock_server);
-        return -1;
-    }
-
-    return sock_server;
+	std::cout << "New client connected on descriptor " << sock_server << "!" << std::endl;
+	
+	event.events = EPOLLIN;
+	event.data.fd = sock_server;
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock_server, &event) == -1) {
+		perror("epoll_ctl: sock_server");
+		close(sock_server);
+		return -1;
+	}
+	return sock_server;
 }
 
 // Global or class-level state to keep track of ongoing requests jst for prototyping
@@ -157,36 +174,36 @@ int accept_new_client(int epoll_fd, int sock_listen) {
 std::map<int, HttpRequest> ongoingRequests;
 
 void handle_client_data(int epoll_fd, int client_fd) {
-    
+	
 	
 	(void)epoll_fd; //we should use this variable to handle the error
 	std::cout << "Receiving data from client on descriptor " << client_fd << std::endl;
 	
 	
-    if (ongoingRequests.find(client_fd) == ongoingRequests.end()) {
-        // New client, create a HttpRequest for it
-        ongoingRequests[client_fd] = HttpRequest();
-    }
+	if (ongoingRequests.find(client_fd) == ongoingRequests.end()) {
+		// New client, create a HttpRequest for it
+		ongoingRequests[client_fd] = HttpRequest();
+	}
 
-    HttpRequest& request = ongoingRequests[client_fd];
-    
+	HttpRequest& request = ongoingRequests[client_fd];
+	
 	try {
-        request.recv(client_fd);
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << '\n';
-        close(client_fd);
+		request.recv(client_fd);
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << '\n';
+		close(client_fd);
 		ongoingRequests.erase(client_fd);
-        throw;  // return would be beter	
-    }
+		throw;  // return would be beter	
+	}
 
-    if (request.isComplete()) {
-        std::cout << "before respond" << std::endl;
-        request.respond(client_fd, "200");
-        std::cout << "before clear" << std::endl;
-        request.clear();
-        close(client_fd);
-        ongoingRequests.erase(client_fd);
-    }
+	if (request.isComplete()) {
+		std::cout << "before respond" << std::endl;
+		request.respond(client_fd, "200");
+		std::cout << "before clear" << std::endl;
+		request.clear();
+		close(client_fd);
+		ongoingRequests.erase(client_fd);
+	}
 	else {
 		std::cout << "Request not complete yet" << std::endl;
 	}
@@ -205,61 +222,66 @@ and use that as the timeout. If there's nothing immediate to do, it might block
 indefinitely.
 */
 int calculate_dynamic_timeout() {
-    // Logic to determine appropriate timeout
+	// Logic to determine appropriate timeout
 	int timeout_value = 1000;
-    return timeout_value;
+	return timeout_value;
 }
 
-void handle_epoll_events(int epoll_fd, int sock_listen) {
-    
+int handle_epoll_events(int epoll_fd, int sock_listen) {
+	
 	struct epoll_event	events[MAX_EVENTS];
-    HttpRequest			request;
-    int					num_fds;
-
+	HttpRequest			request;
+	int					num_fds;
+	int					sock_server;
 
 	int timeout = calculate_dynamic_timeout();
 
 	num_fds = epoll_wait(epoll_fd, events, MAX_EVENTS, timeout);
-    if (num_fds == -1) {
-        perror("epoll_wait");
-        exit(EXIT_FAILURE);
-    }
+	if (num_fds == -1) {
+		return perror("epoll_wait"), -1;
+	}
 
-    if (num_fds == 0) {
-        std::cout << "Waiting for new clients or data from existing clients..." << std::endl;
-        return;
-    }
+	if (num_fds == 0) {
+		std::cout << "Waiting for new clients or data from existing clients..." << std::endl;
+		return 1;
+	}
 
-    for (int i = 0; i < num_fds; i++) {
-        if (events[i].data.fd == sock_listen) {
-            if (accept_new_client(epoll_fd, sock_listen) == -1) {
-                continue;
-            }
-        } else {
-            handle_client_data(epoll_fd, events[i].data.fd);
-        }
-    }
+	for (int i = 0; i < num_fds; i++) {
+		if (events[i].data.fd == sock_listen) {
+			sock_server = accept_new_client(epoll_fd, sock_listen);
+			if (sock_server == -1) {
+				continue;
+			}
+		} else {
+			handle_client_data(epoll_fd, events[i].data.fd);
+		}
+	}
+	return 0;
 }
-
 
 int main(void) {
 
-    int						sock_listen;
+	int						sock_listen;
 	int						epoll_fd;
 
+	signal(SIGINT, signal_handler);
 	if (setUpSocket(&sock_listen) == -1) {
-        return 2;
+		return 2;
 	}
-	
-    listen(sock_listen, BACKLOG);
-	
-	epoll_fd = setUpEpoll(sock_listen);
 
-    while (true) {
-        handle_epoll_events(epoll_fd, sock_listen);
-    }
-	
-	close(epoll_fd);
+	listen(sock_listen, BACKLOG);
+
+	epoll_fd = setUpEpoll(sock_listen);
+	std::cout << epoll_fd << std::endl;
+
+	if (epoll_fd != -1) {
+		while (run) {
+			if (handle_epoll_events(epoll_fd, sock_listen) == -1)
+				break;
+		}
+		std::cout << epoll_fd << std::endl;
+		close(epoll_fd);
+	}
 	close(sock_listen);
 	
 	return 0;
