59abb0f43a9f905b306c0d7e1798e7cb5fcba010
motero <motero@student.42.fr>
2023-08-17 18:20:05 +0200

    placeholder methods in server.cpp file

--- sources/Server.cpp
+++ sources/Server.cpp
@@ -6,26 +6,56 @@
 /*   By: motero <motero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 01:18:42 by rgarrigo          #+#    #+#             */
-/*   Updated: 2023/08/17 18:17:03 by motero           ###   ########.fr       */
+/*   Updated: 2023/08/17 18:19:48 by motero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
+    Server::Server() {
+        
+    }
+    
+    Server::Server(const Server& other) {
+        
+    }
+    
+    Server &Server::operator=(const Server& other) {
+        
+    }
+    
+    Server::~Server() {
+        
+    }
+    
 
-   
-    void    setUpSocket(int port);
-    int     setUpEpoll(int sock_listen);
-    int     accept_new_client(int sock_listen);
-    void    handle_client_data(int client_fd);
+    void    Server::setUpSocket(int port) {
+        
+    }
+    
+    int     Server::setUpEpoll(int sock_listen) {
+        
+    }
+    
+    int     Server::accept_new_client(int sock_listen) {
+        
+    }
+    
+    void    Server::handle_client_data(int client_fd) {
+        
+    }
+    
 
-    Server();
-    Server(const Server& other);
-    Server& operator=(const Server& other);
-    ~Server();
+    void Server::start(){
+        
+    }
+    
+    void Server::stop(){
+        
+    }
+    
 
-    void start();
-    void stop();
-
-    void loadConfig(const std::string& configPath);
+    void Server::loadConfig(const std::string& configPath) {
+        
+    }
     std::ostream& operator<<(std::ostream& os, const Server & server);
