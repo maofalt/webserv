2aba49c3fd340e141b46f39adb1d6204ea65807b
motero <motero@student.42.fr>
2023-08-30 19:25:34 +0200

    we verify if a sections is empty wihtout any parameres and we erase it if it s the case

--- sources/IniParser.cpp
+++ sources/IniParser.cpp
@@ -6,7 +6,7 @@
 /*   By: motero <motero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 16:11:41 by motero            #+#    #+#             */
-/*   Updated: 2023/08/30 19:02:28 by motero           ###   ########.fr       */
+/*   Updated: 2023/08/30 19:25:02 by motero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int IniParser::loadConfig(const std::string& filename) {
     std::ifstream   inFile(filename.c_str());
     std::string     line;
     std::string     currentSection;
+    std::string     previousSection;
 
     if (!inFile.is_open()) {
         throw std::runtime_error("Failed to open the INI file");
@@ -59,6 +60,11 @@ int IniParser::loadConfig(const std::string& filename) {
                 _errorInSection = false;
             }
             
+            if (!previousSection.empty() && data[previousSection].empty()) {
+                log_message(Logger::WARN, "Section %s is empty. Erasing section.", previousSection.c_str());
+                data.erase(previousSection);
+            }
+            previousSection = currentSection;
             handleSection(line, currentSection);
         } else {
             handleKeyValuePair(line, currentSection);
@@ -69,6 +75,10 @@ int IniParser::loadConfig(const std::string& filename) {
     if (_errorInSection) {
         data.erase(currentSection);
         currentSection.clear();
+    }
+    if (!previousSection.empty() && data[previousSection].empty()) {
+        log_message(Logger::WARN, "Section %s is empty. Erasing section.", previousSection.c_str());
+        data.erase(previousSection);
     }
     
     inFile.close();
