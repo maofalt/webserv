447508331f8ad0ea8eb2849b5b87fdf3b69365af
Romain GARRIGOU <rgarrigo@e1r10p10.clusters.42paris.fr>
2023-09-03 02:18:17 +0200

    Correct type is php script

--- site2/php/myNameIs.php
+++ site2/php/myNameIs.php
@@ -6,7 +6,10 @@
 
 <body>
 	<div>
-		<?php echo 'Your name is <b>' . $_GET["name"] . '</b>!';?>
+		<?php if (isset($_GET["firstname"]) && isset($_GET["firstname"]))
+			echo 'Your name is <b>' . $_GET["firstname"] . " " . $_GET["surname"] . '</b>!';
+		else
+			echo 'Tell me your name by appending the url with: "?firstname=*yourFirstName*&surname=*yourSurname*"'?>
 	</div>
 </body>
 </html>
