946a218e943ee2375f39a5f87c774be94b6ac8a0
motero <motero@student.42.fr>
2023-09-15 16:21:06 +0200

    fixed new header files and they compile

--- includes/validationStrategy/isPositiveInteger.hpp
+++ includes/validationStrategy/isPositiveInteger.hpp
@@ -1,3 +1,6 @@
+#ifndef ISPOSITIVEINTEGER_HPP
+#define ISPOSITIVEINTEGER_HPP
+
 #include "ValidationStrategy.hpp"
 
 class isPositiveInteger : public ValidationStrategy {
@@ -5,3 +8,5 @@ public:
     virtual bool validate(const std::string& value, const std::map<std::string, std::string>& fieldProperties) const;
     virtual bool validate(const std::vector<std::string>& values, const std::map<std::string, std::string>& fieldProperties) const;
 };
+
+#endif // ISPOSITIVEINTEGER_HPP
