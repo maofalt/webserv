eade2cd4c1b2703707e374bf8b42103d29ed4899
motero <motero@student.42.fr>
2023-09-15 16:46:42 +0200

    validation strategy refactorised

--- sources/validationStrategy/isValidFileUploadTypes.cpp
+++ sources/validationStrategy/isValidFileUploadTypes.cpp
@@ -6,7 +6,7 @@
 /*   By: motero <motero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 21:19:07 by motero            #+#    #+#             */
-/*   Updated: 2023/09/15 16:26:21 by motero           ###   ########.fr       */
+/*   Updated: 2023/09/15 16:45:08 by motero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,38 @@ bool isValidFileUploadTypes::validate(const std::vector<std::string>& values, co
     return true;
 }
 
-bool isValidFileUploadTypes::validate(const std::string& value, const std::map<std::string, std::string>& fieldProperties) const {
-    
-    (void)fieldProperties; // Unused
-    // Extract valid types from fieldProperties or use a hardcoded set.
+std::set<std::string> isValidFileUploadTypes::getValidTypes() const {
     std::set<std::string> validTypes;
     validTypes.insert("jpg");
     validTypes.insert("jpeg");
     validTypes.insert("png");
     validTypes.insert("gif");
-    // Split the 'value' by commas or spaces, if you expect multiple values.
+    return validTypes;
+}
+
+std::vector<std::string> isValidFileUploadTypes::splitValue(const std::string& value) const {
     std::vector<std::string> givenTypes;
     std::stringstream ss(value);
     std::string item;
-    while (std::getline(ss, item, ',')) { // Assuming ',' as delimiter.
+    while (std::getline(ss, item, ',')) {
         givenTypes.push_back(item);
     }
-    // Check each given type against validTypes.
-    for (std::vector<std::string>::iterator type = givenTypes.begin(); type != givenTypes.end(); ++type) {
+    return givenTypes;
+}
+
+bool isValidFileUploadTypes::checkAgainstValidTypes(const std::vector<std::string>& givenTypes, const std::set<std::string>& validTypes) const {
+    for (std::vector<std::string>::const_iterator type = givenTypes.begin(); type != givenTypes.end(); ++type) {
         if (validTypes.find(*type) == validTypes.end()) {
             throw std::invalid_argument("Invalid file type: " + *type);
-            return false; // Found an invalid type.
         }
     }
-    return true; // All given types are valid.
+    return true;
+}
+
+bool isValidFileUploadTypes::validate(const std::string& value, const std::map<std::string, std::string>& fieldProperties) const {
+    (void)fieldProperties; // Unused
+    std::set<std::string> validTypes = getValidTypes();
+    std::vector<std::string> givenTypes = splitValue(value);
+
+    return checkAgainstValidTypes(givenTypes, validTypes);
 }
