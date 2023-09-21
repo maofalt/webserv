576a29adfcf24f1b5c9b614ddd1c826453aa0123
motero <motero@student.42.fr>
2023-09-21 18:37:29 +0200

    now erros wirte correctly the line and fonction

--- includes/Utils.hpp
+++ includes/Utils.hpp
@@ -11,6 +11,8 @@
 
 //int to string
 std::string     intToStr(int num);
-void            log_message(Logger::LogLevel level, const char* format, ...); 
+void log_message_impl(Logger::LogLevel level, const char* file, int line, const char* format, ...);
+
+#define log_message(level, format, ...) log_message_impl(level, __FILE__, __LINE__, format, ##__VA_ARGS__)
 
 #endif // UTILS_HPP
