aa91a54ca9238f4f0f277d2783415a559069c021
Romain GARRIGOU <rgarrigo@e1r10p10.clusters.42paris.fr>
2023-09-21 18:08:47 +0200

    Python Cgi and sublime writeDirectory

---
+++ site2/python/show_cookies.py
@@ -0,0 +1,32 @@
+from os import environ
+import cgi, cgitb
+
+print ("""<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Webserv</title>
+	<link rel="stylesheet" href="css/style_index.css" />
+	<link rel="icon\" type="image/png" href="motero.png" />
+</head>
+
+<body>
+	<header>
+		<h1>Your cookies</h1>
+		<ul>""")
+
+try:
+	for cookie in map(str.strip, environ['HTTP_COOKIE'].split(';')):
+		print ("			<li>%s</li>" % cookie)
+except KeyError:
+	print("			<p>No cookies, sad.</p>")
+
+print ("""		</ul>
+	</header>
+
+	<footer>
+		<p>Rgarrigo&nbsp;&nbsp;&nbsp;Motero&nbsp;&nbsp;&nbsp;Znogueir</p>
+	</footer>
+</body>
+
+</html>""")
